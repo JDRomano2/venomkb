cb4d9e09aa6672f3596cc169e8498080025adaa5
Joe Romano <JDRomano2@gmail.com>
2018-05-22 18:40:48 -0400

    Typo error in date

--- index/components/Home.js
+++ index/components/Home.js
@@ -21,7 +21,7 @@ const Home = () =>
 			<h3>News and Updates</h3>
 			<ul id="news-items">
 				<li id="news-item">
-					<b>May 22, 2017:</b> VenomKB has undergone a major makeover under the hood! You can expect the site to be faster and more responsive. For more details, check out <a href="https://github.com/JDRomano2/venomkb/releases/tag/2.0.1-alpha">the 2.0.1-alpha release on Github</a>. We are currently working on major upgrades to the API; stay tuned!
+					<b>May 22, 2018:</b> VenomKB has undergone a major makeover under the hood! You can expect the site to be faster and more responsive. For more details, check out <a href="https://github.com/JDRomano2/venomkb/releases/tag/2.0.1-alpha">the 2.0.1-alpha release on Github</a>. We are currently working on major upgrades to the API; stay tuned!
 				</li>
 				<li id="news-item">
 					<b>October 16, 2017:</b> We've added disease/condition annotations to proteins in VenomKB. Head over to <a href="/data">venomkb.org/data</a> to give it a try.
