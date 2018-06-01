8b16451ddc6f32ed671480c9c6b08c434089a813
Saint Mézard Marine <marine.saint-mezard@ensta-paristech.fr>
2018-06-01 12:08:19 -0400

    add methods and routes for genomes

--- index/api/models/Genome.js
+++ index/api/models/Genome.js
@@ -13,4 +13,76 @@ const GenomeSchema = new mongoose.Schema({
 });
 
 
-module.exports = mongoose.model('Genome', GenomeSchema);
+const Genome = mongoose.model('Genome', GenomeSchema);
+
+/**
+ * returns all the genomes
+ */
+Genome.getAll = () => {
+    return new Promise((resolve, reject) => {
+        Genome.find({})
+            .exec((err, genomes) => {
+                if (err) reject(err)
+                resolve(genomes)
+            })
+    })
+}
+
+/**
+ * Get an genome given its venomkb_id
+ * @param {ObjectId} id  id of the genome to get
+ * @param {String} path path to populate (leave blank if none)
+ */
+Genome.getByVenomKBId = (venomkb_id, path) => {
+    return new Promise((resolve, reject) => {
+        Genome.findOne({ venomkb_id: venomkb_id })
+            .populate(path || '')
+            .exec((err, genome) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(genome);
+            });
+    });
+};
+
+
+/**
+ * Get an genome given its id
+ * @param {ObjectId} id  id of the genome to get
+ * @param {String} path path to populate (leave blank if none)
+ */
+Genome.getById = (id, path) => {
+    return new Promise((resolve, reject) => {
+        Genome.findOne({ _id: id })
+            .populate(path || '')
+            .exec((err, genome) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(genome);
+            });
+    });
+};
+
+/**
+ * Get an genome given its name
+ * @param {String} name  name of the genome to get
+ * @param {String} path path to populate (leave blank if none)
+ */
+Genome.getByName = (name, path) => {
+    return new Promise((resolve, reject) => {
+        Genome.find({ $text: { $search: name } })
+            .populate(path || '')
+            .exec((err, genomes) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log(genomes.length);
+
+                resolve(genomes);
+            });
+    });
+};
+
+module.exports = Genome;
--- index/api/routes/genomes.js
+++ index/api/routes/genomes.js
@@ -2,26 +2,23 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const genome = require('../models/Genome.js');
+const Genome = require('../models/Genome.js');
 
 const vkbid_reg = /G\d{7}/;
 
+/**
+ * Get a list of all genomes
+ * @returns an array of genome object
+ */
 /* GET /genomes listing. */
 router.get('/', (req, res, next) => {
-  //genome.find({}).sort({ updatedAt: -1 }).exec((err, genomes) => {
-  genome.find({}).exec((err, genomes) => {
-    if (err) return next(err);
-    res.json(genomes);
-  });
-});
-
-/* POST /genomes */
-router.post('/', (req, res, next) => {
-  genome.create(req.body,  (err, genomes) => {
-    if (err) return next(err);
-    console.log('New genome created:');
-    console.log(genomes);
-    res.json(genomes);
-  });
+  Genome.getAll()
+    .then(genomes => {
+      res.json(genomes)
+    })
+    .catch(err => {
+      return sendStatusMessage(res, 500, err.message);
+    })
 });
 
 /* GET /genomes/index */
@@ -32,22 +29,66 @@ router.get('/index', (req, res, next) => {
   });
 });
 
+/**
+ * Find all genome that have a given pattern in their name
+ * @param {Query} name full name or part of the name of the genome
+ * @returns the genome if only one result, an array of genome object in other case
+ */
+/* GET /genomes/name */
+router.get('/search', (req, res, next) => {
+  if (!req.query.name) {
+    console.log("You must enter a name");
+    return utils.sendStatusMessage(res, 400, "genome name not specified")
 
+  }
+  console.log("Find by name");
+  Genome.getByName(req.query.name)
+    .then(genome => {
+      res.json(genome)
+    })
+    .catch(err => {
+      return sendStatusMessage(res, 500, err.message);
+    })
+});
+
+/**
+ * Find a genome given its id or venomkb_id
+ * @param {Params} id object id or venomkb_id of the genome
+ * @returns the genome
+ */
 /* GET /genomes/id */
 router.get('/:id', (req, res, next) => {
+  if (!req.params.id) {
+    return utils.sendStatusMessage(res, 400, "genome id not specified")
+  }
   if (vkbid_reg.test(req.params.id)) {
     console.log("Find by VenomKB id");
-    genome.find({ 'venomkb_id': req.params.id }, (err, genome) => {
-      if (err) return handleError(err);
-      res.json(genome);
-    });
+    Genome.getByVenomKBId(req.params.id)
+      .then(genome => {
+        res.json(genome)
+      })
+      .catch()
   } else {
     console.log("Find by id");
-    genome.findById(req.params.id, (err, genomes) => {
-      if (err) return next(err);
-      res.json(genomes);
-    });
+    Genome.getById(req.params.id)
+      .then(genome => {
+        res.json(genome)
+      })
+      .catch(err => {
+        return sendStatusMessage(res, 500, err.message);
+      })
   }
+});
+
+
+/* POST /genomes */
+router.post('/', (req, res, next) => {
+  genome.create(req.body,  (err, genomes) => {
+    if (err) return next(err);
+    console.log('New genome created:');
+    console.log(genomes);
+    res.json(genomes);
+  });
 });
 
 /* PUT /genomes/:id */
