9cd0fb34d838ac823470de9c84781088ace25e0b
Joseph Romano <JDRomano2@gmail.com>
2018-06-02 18:50:15 -0400

    Make testing setup work

--- test/test.js
+++ test/test.js
@@ -1,24 +1,15 @@
+import chai, { expect } from 'chai';
 import React from 'react';
-import { expect } from 'chai';
-import { mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { spy } from 'sinon';
-import App from '../index/components/App';
 
-// Making sure the test library works
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
-    });
-});
+import About from '../index/components/About';
 
-// React automatic function calls for components
-spy(App.prototype, 'componentDidMount');
+Enzyme.configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-    it('calls componentDidMount', () => {
-        const wrapper = mount(<App />);
-        expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+describe('Mocha', () => {
+    it('passes a trivial test', () => {
+        expect(true).to.equal(true);
     });
 });
