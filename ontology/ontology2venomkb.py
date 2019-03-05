22650c188a5e7b0a3736ad067e8391a99bd98eb9
Joseph D. Romano <jdromano2@gmail.com>
2019-03-05 15:22:06 -0500

    Begin to build final JSON structure for proteins and species

--- ontology/ontology2venomkb.py
+++ ontology/ontology2venomkb.py
@@ -20,7 +20,9 @@ INDIVIDUALS = list(onto.individuals())
 # TODO: these should be determined dynamically - it breaks when class hierarchy is changed
 PROTEIN_CLASS = CLASSES[6]
 SPECIES_CLASS = CLASSES[7]
+NOMENCLATURE_CHANGES = []
 
+# Set up SPARQL wrapper and template for retrieving old protein names from UniProtKB
 SPARQL = SPARQLWrapper("https://sparql.uniprot.org/sparql")
 ONM_TEMPLATE = """PREFIX up:<http://purl.uniprot.org/core/>
 SELECT ?onm
@@ -29,17 +31,27 @@ WHERE {{
     ?protein up:oldMnemonic ?onm .
     ?protein up:mnemonic '{new_mnemonic}'
 }}"""
+OLD_MNEMONIC_CACHE_FNAME = "./data/old_mnemonics.pkl"
+if os.path.isfile(OLD_MNEMONIC_CACHE_FNAME):
+  with open(OLD_MNEMONIC_CACHE_FNAME, 'rb') as fp:
+    mnemonic_cache = pickle.load(fp)
+else:
+  mnemonic_cache = {}
 
 def fetch_old_mnemonics(new_name):
-  SPARQL.setQuery(ONM_TEMPLATE.format(new_mnemonic=new_name))
-  SPARQL.setReturnFormat(JSON)
-  res = SPARQL.query().convert()
-  old_mnemonics = []
-  for result in res['results']['bindings']:
-    old_mnemonics.append(result['onm']['value'])
-  return old_mnemonics
+  if new_name in mnemonic_cache.keys():
+    return mnemonic_cache[new_name]
+  else:
+    SPARQL.setQuery(ONM_TEMPLATE.format(new_mnemonic=new_name))
+    SPARQL.setReturnFormat(JSON)
+    res = SPARQL.query().convert()
+    old_mnemonics = []
+    for result in res['results']['bindings']:
+      old_mnemonics.append(result['onm']['value'])
+    mnemonic_cache[new_name] = old_mnemonics
+    return old_mnemonics
+# E.g., fetch_old_mnemonics("H16A7_CYRHA")
 
-# E.g., H16A7_CYRHA
 
 print("Fetching current data from VenomKB API.")
 VKB_PROTS = json.load(urlopen("http://venomkb.org/api/proteins"))
@@ -54,6 +66,15 @@ def map_upacc_to_upname(accession):
   entry_strio = StringIO(tmp.read().decode("utf-8"))
   tab = pd.read_table(entry_strio)
   return(tab['Entry name'][0])
+
+def add_incompatibility(obj, text):
+  if 'incompatibility' in obj.keys():
+    obj['incompatibility'].append(text)
+  else:
+    obj['incompatibility'] = [text]
+
+def normalize_species_name(species_name):
+  return species_name.lower().replace(" ", "_")
 
 
 depr = {
@@ -78,6 +99,11 @@ for index, row in toxprot_meta.iterrows():
   # Need to look for deprecated names!
   acc2name[curr[0]] = curr[1]
 
+
+###########################################
+# READ MOST OF THE DATA WE NEED INTO MEMORY
+###########################################
+
 # Start with Proteins as the fundamental datatype
 print("Building \"Protein\" and \"Species\" objects")
 all_proteins = []
@@ -85,6 +111,8 @@ for i in INDIVIDUALS:
   if i.is_a[0] == PROTEIN_CLASS:
     all_proteins.append(i)
 
+# First pass: 
+# Make bare proteins and species with object annotation links
 proteins_composed = {}
 species_composed = defaultdict(species_factory)
 for p in all_proteins:
@@ -98,9 +126,11 @@ for p in all_proteins:
   proteins_composed[p.name] = this_protein
 
 
-# Link current venomkb data to ontology output
+# Second pass:
+# Read in existing VenomKB data and add to the bare protein and species data structures
 print("Merging existing ontology contents into data structures")
-for v_p in VKB_PROTS:
+print("  ...proteins")
+for v_p in tqdm(VKB_PROTS):
   try:
     p_name = acc2name[v_p['out_links']['UniProtKB']['id']]
   except KeyError:
@@ -109,22 +139,67 @@ for v_p in VKB_PROTS:
   try:
     p_equiv = proteins_composed[p_name_undepr]
     p_equiv['vkb_legacy'] = v_p
+    if p_name != p_name_undepr:
+      add_incompatibility(p_equiv, "UniProt species nomenclature changed: {0} -> {1}".format(p_name, p_name_undepr))
+      NOMENCLATURE_CHANGES.append("Protein name changed: {0} -> {1}".format(p_name, p_name_undepr))
   except KeyError:
-    print("Error: {0}".format(p_name_undepr))
+    #print("Error: {0}".format(p_name_undepr))
     # Check for deprecated names
     old_mnemonics = fetch_old_mnemonics(p_name_undepr)
     for omn in old_mnemonics:
       if omn in proteins_composed.keys():
         p_equiv = proteins_composed[omn]
         p_equiv['vkb_legacy'] = v_p
+        add_incompatibility(p_equiv, "Protein mnemonic changed: {0} -> {1}".format(p_name, omn))
+        NOMENCLATURE_CHANGES.append("Protein name changed: {0} -> {1}".format(p_name, omn))
+print("  ...species")
+for v_s in VKB_SPECS:
+  v_s_name = v_s['name']
+  normalized_name = normalize_species_name(v_s_name)
+  s_equiv = species_composed[normalized_name]
+  s_equiv['vkb_legacy'] = v_s
 
-# TODO: add taxonomy to species
 
-# TODO: add publications to proteins, copy to species as well
-# TODO: add predications to proteins, copy to species as well
+########################################################
+# NOW, WE CAN PLACE DATA WHERE WE ACTUALLY WANT IT TO GO
+########################################################
+
+# START BUILDING THE JSON OBJECT (as a python dict for now)
+
+# PROTEINS FIRST
+for pr_n, pr in proteins_composed.items():
+  vkbl = pr['vkb_legacy']
+  pr['json'] = {
+
+  }
+
+
+def get_prot_vkbids(prot_name_list):
+  vkbids = []
+  for pn in prot_name_list:
+    vkbids.append(proteins_composed[pn]['vkb_legacy']['venomkb_id'])
+  return vkbids
+
+# THEN SPECIES
+for sp_n, sp in species_composed.items():
+  vkbl = sp['vkb_legacy']
+  sp['json'] = {
+    'speciesName': sp_n,
+    'commonName': vkbl['common_name'],
+    'venomkbId': vkbl['venomkb_id'],
+    'venomRef': vkbl['venom_ref'],
+    'venom': {
+      'proteins': get_prot_vkbids(prot_names)
+    },
+
+  }
 
 # TODO: Add GO annotations
 
-# TODO: Fetch other external IDs and compose URLs
+# TODO: Fetch other external IDs and compose "outLinks"
+# (use predefined rules to construct URLs)
 
-# TODO: Dump data to JSON and write to disk
+# TODO: Dump data to JSON and write files to disk
+print("Dumping cache of deprecated names to disk.")
+with open(OLD_MNEMONIC_CACHE_FNAME, 'wb') as fp:
+  pickle.dump(mnemonic_cache, fp)
