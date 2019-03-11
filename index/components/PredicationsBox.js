d06f0ea940a267e70f6d004da2197d35bb1a411b
Joseph Romano <jdromano2@gmail.com>
2019-03-11 11:10:33 -0400

    Add client-side deduplication of literature predications

--- index/components/PredicationsBox.js
+++ index/components/PredicationsBox.js
@@ -3,9 +3,53 @@ import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import ReactTooltip from 'react-tooltip';
 
+// GOOD EXAMPLE OF DUPLICATES: P9587088
 class PredicationsBox extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            predications: props.predications,
+            predsDedup: this.collapseDuplicateRows(props.predications)
+        };
+
+        //this.collapseDuplicateRows = this.collapseDuplicateRows.bind(this);
+
+    }
+
+    collapseDuplicateRows(preds) {
+
+        var predsDedup = []
+        var seen = []
+
+        for (var i=0; i < preds.length; i++) {
+            // fetch relevant components
+            const subPred = {
+                s_name: preds[i].s_name,
+                predicate: preds[i].predicate,
+                o_name: preds[i].o_name,
+                pmid: preds[i].pmid
+            }
+            // compose identifier
+            var ident = `${subPred.s_name}${subPred.predicate}${subPred.o_name}`;
+
+            if (predsDedup.hasOwnProperty(ident)) {
+                // We've already seen this predication
+                predsDedup[ident].pmid.push(subPred.pmid);
+            } else {
+                // We haven't seen it yet
+                predsDedup[ident] = {
+                    s_name: subPred.s_name,
+                    predicate: subPred.predicate,
+                    o_name: subPred.o_name,
+                    pmid: [subPred.pmid] // HERE!
+                }
+            }
+        }
+
+        // Now convert it to an array:
+        const predsDedupValues = Object.keys(predsDedup).map(function(k){return predsDedup[k]});
+        return predsDedupValues;
     }
 
     render() {
@@ -23,8 +67,7 @@ class PredicationsBox extends React.Component {
             accessor: 'pmid'
         }];
 
-        console.log("predications:");
-        console.log(this.props.predications);
+        console.log(this.state);
 
         return (
             <div className="jdr-box">
@@ -34,15 +77,15 @@ class PredicationsBox extends React.Component {
                     data-tip="Go to About > Predications for more information"
                 />
                 <ReactTooltip />
-                { !(this.props.predications === undefined) &&
+                { !(this.state.predsDedup === undefined) &&
                 <ReactTable
-                    data={this.props.predications}
+                    data={this.state.predsDedup}
                     columns={columns}
                     showPagination={false}
-                    defaultPageSize={this.props.predications.length}
+                    defaultPageSize={this.state.predsDedup.length}
                 />
                 }
-                { (this.props.predications === undefined) &&
+                { (this.state.predsDedup === undefined) &&
                 <div>
                     No predications present in VenomKB for this record.
                 </div>
