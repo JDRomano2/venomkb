56d1af39ae118946e397ebe6d4f7ab6dd77131a4
Joseph Romano <jdromano2@gmail.com>
2019-04-12 20:46:46 -0400

    Add extra data to drugs

--- index/components/DerivedDrug.js
+++ index/components/DerivedDrug.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ButtonToolbar, Button, Media } from 'react-bootstrap';
 
 class DerivedDrug extends React.Component {
   constructor(props) {
@@ -36,6 +37,7 @@ class DerivedDrug extends React.Component {
       atcLink: atcLink,
       drugbankLink: drugbankLink,
       rxnormLink: rxnormLink,
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Exenatide.svg/175px-Exenatide.svg.png',
     };
   }
 
@@ -46,19 +48,44 @@ class DerivedDrug extends React.Component {
 
     return (
       <div id='drug-box'>
-        <h4>Drug name: {this.state.drugName}</h4>
-        <p>
-          {this.state.description}
-        </p>
-        <p>
-          ATC code: <a href={atcUrl} target='_blank'>{this.state.atcLink['identifier']}</a>
-        </p>
-        <p>
-          Drugbank ID: <a href={drugbankUrl} target='_blank'>{this.state.drugbankLink['identifier']}</a>
-        </p>
-        <p>
-          RxNorm RXCUI: <a href={rxnormUrl} target='_blank'>{this.state.rxnormLink['identifier']}</a>
-        </p>
+        <Media>
+          <Media.Body>
+            <Media.Heading>Drug name: {this.state.drugName}</Media.Heading>
+            <p>
+              Synonyms and brands: {this.state.brandNames.join(', ')}
+            </p>
+            <p>
+              {this.state.description}
+            </p>
+
+            <ButtonToolbar className='drug-button-toolbar'>
+              <Button
+                href={atcUrl}
+                target='_blank'
+                bsStyle='primary'>
+                  ATC code: {this.state.atcLink['identifier']}
+              </Button>
+              <Button
+                href={drugbankUrl}
+                target='_blank'
+                bsStyle='primary'>
+                  Drugbank ID: {this.state.drugbankLink['identifier']}
+              </Button>
+              <Button
+                href={rxnormUrl}
+                target='_blank'
+                bsStyle='primary'>
+                  RxNorm RXCUI: {this.state.rxnormLink['identifier']}
+              </Button>
+            </ButtonToolbar>
+          </Media.Body>
+
+          <Media.Right>
+            <img src={this.state.image_url} width={175} height={240}/>
+          </Media.Right>
+        </Media>
+
+
       </div>
     );
   }
